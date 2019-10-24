10875a70fd73d43918740198dacbfe2ce1fe41c4
Yoav Navon <yanavon@uc.cl>
2019-10-24 20:08:21 -0300

    Fix: Add space for go to top button

--- src/screens/AddStops/index.js
+++ src/screens/AddStops/index.js
@@ -13,6 +13,7 @@ import './style.sass'
 
 // utils
 import { spotsFilter } from '../../utils/spotsFilter'
+import { MAX_MIDDLE_STOPS } from '../../constants'
 
 // reducers
 import { getAllSpots } from '../../redux/actions/spots'
@@ -23,18 +24,34 @@ import {
 } from '../../redux/actions/createtrip'
 
 class AddStopsScreen extends Component {
-  propTypes = {
-    spots: PropTypes.string.isRequired,
-    endStop: PropTypes.string.isRequired,
-    startStop: PropTypes.string.isRequired,
-    middleStops: PropTypes.array.isRequired,
-    startTime: PropTypes.array.isRequired,
-    addMiddleStop: PropTypes.func.isRequired,
-    removeMiddleStop: PropTypes.func.isRequired,
-    createTrip: PropTypes.func.isRequired,
+  constructor(props) {
+    super(props)
+    this.createMiddleStop = this.createMiddleStop.bind(this)
   }
+
   componentDidMount = () => {
-    this.props.getAllSpots()
+    this.props.getAllSpots() // TODO unnecesary call if comming from createTrip screen
+  }
+
+  createMiddleStop(stop, index) {
+    return (
+      <Grid item xs={12} container>
+        <Grid item xs={10}>
+          <StopCard
+            label="Parada"
+            number={index + 1}
+            stopName={stop.name}
+          ></StopCard>
+        </Grid>
+        <Grid item xs={2}>
+          <Box mt={2} ml={1}>
+            <IconButton onClick={() => this.props.removeMiddleStop(stop)}>
+              <ClearIcon />
+            </IconButton>
+          </Box>
+        </Grid>
+      </Grid>
+    )
   }
 
   render() {
@@ -47,67 +64,56 @@ class AddStopsScreen extends Component {
       label: `${spot.name}, ${spot.address}`,
       value: spot,
     }))
-    const middleStopsComponents = middleStops.map((stop, i) => {
-      return (
-        <Grid item container key={i}>
-          <Grid item xs={10}>
-            <StopCard
-              label="Parada"
-              number={i + 1}
-              stopName={stop.name}
-            ></StopCard>
-          </Grid>
-          <Grid item xs={2}>
-            <Box mt={2} ml={1}>
-              <IconButton onClick={() => this.props.removeMiddleStop(stop)}>
-                <ClearIcon />
-              </IconButton>
-            </Box>
-          </Grid>
-        </Grid>
-      )
-    })
+    const middleStopsComponents = middleStops.map(this.createMiddleStop)
     return (
-      <Grid container direction="column" justify="center" alignItems="center">
-        <Grid item container>
-          <Grid item xs={12}>
-            <StopCard label="Desde" stopName={startStop.name}></StopCard>
-          </Grid>
+      <Grid container justify="flex-start" alignItems="center">
+        <Grid item xs={12}>
+          <StopCard label="Desde" stopName={startStop.name}></StopCard>
         </Grid>
-        <Grid item container>
-          <Grid item xs={12}>
-            <StopCard label="Hasta" stopName={endStop.name}></StopCard>
-          </Grid>
+        <Grid item xs={12}>
+          <StopCard label="Hasta" stopName={endStop.name}></StopCard>
         </Grid>
-
         {middleStopsComponents}
-        <Typography>
-          <p> Agrega paradas extra (opcional)</p>
-        </Typography>
-        <Select
-          className="search"
-          isSearchable={true}
-          options={filteredSpots}
-          onChange={option => this.props.addMiddleStop(option.value)}
-        />
-        <Button
-          color="primary"
-          onClick={() =>
-            this.props.createTrip(
-              [startStop, ...middleStops, endStop],
-              startTime
-            )
-          }
-        >
-          Crear Viaje
-        </Button>
+        <Grid item xs={10}>
+          <Typography>
+            <p> Agrega paradas extra (opcional)</p>
+          </Typography>
+          <Select
+            className="search"
+            isSearchable={true}
+            isDisabled={middleStops.length >= MAX_MIDDLE_STOPS}
+            options={filteredSpots}
+            onChange={option => this.props.addMiddleStop(option.value)}
+          />
+        </Grid>
+        <Grid container item xs={10} justify="center">
+          <Button
+            color="primary"
+            onClick={() =>
+              this.props.createTrip(
+                [startStop, ...middleStops, endStop],
+                startTime
+              )
+            }
+          >
+            Crear Viaje
+          </Button>
+        </Grid>
       </Grid>
     )
   }
 }
 
-AddStopsScreen.navigationOptions = {
-  header: null,
+AddStopsScreen.propTypes = {
+  spots: PropTypes.string.isRequired,
+  endStop: PropTypes.string.isRequired,
+  startStop: PropTypes.string.isRequired,
+  middleStops: PropTypes.array.isRequired,
+  startTime: PropTypes.array.isRequired,
+  addMiddleStop: PropTypes.func.isRequired,
+  removeMiddleStop: PropTypes.func.isRequired,
+  createTrip: PropTypes.func.isRequired,
+  getAllSpots: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = ({ user, createTrip, spots }) => {
@@ -127,10 +133,6 @@ const mapDispatchToProps = {
   removeMiddleStop,
   createTrip,
 }
-
-// TODO: CreateTripScreen.navigationOptions = {
-//   title: 'Crear un viaje',
-// }
 
 export default connect(
   mapStateToProps,
