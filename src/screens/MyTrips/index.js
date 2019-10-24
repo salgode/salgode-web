7c4a411fc765d2174a04f3559d1b1435deab947a
jfcampos1 <jfcampos1@uc.cl>
2019-10-23 22:22:56 -0300

    Feat/my trips async (#17)
    * add: conect to api
    
    * delete: unnesary methods

--- src/screens/MyTrips/index.js
+++ src/screens/MyTrips/index.js
@@ -2,17 +2,14 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
-import { fetchFutureTrips } from '../../redux/actions/trips'
-import './style.sass'
-import MyTripsCard from '../../components/MyTripsCard/index'
-import { ParseDate, ParseHour } from '../../components/ParseDate/index'
-
 // Store
+import { fetchFutureTrips } from '../../redux/actions/trips'
+import MyTripsCard from '../../components/MyTripsCard/index'
+import './style.sass'
 
 // Components
 import { CircularProgress } from '@material-ui/core'
 import Grid from '@material-ui/core/Grid'
-// const MESSAGE = 'Hubo un problema registrandote. Por favor intentalo de nuevo.'
 
 class MyTrips extends Component {
   constructor(props) {
@@ -23,137 +20,36 @@ class MyTrips extends Component {
       trips: [],
     }
     this.getTrips = this.getTrips.bind(this)
-    this.getTrips2 = this.getTrips2.bind(this)
   }
 
   componentDidMount() {
-    this.getTrips(1234)
-    // this.getTrips2()
+    this.getTrips()
   }
 
-  async fetchTrips(token) {
-    // eslint-disable-next-line no-console
-    console.log(token)
-    // fetch from server
-    return [
-      {
-        timestamp: 1571590002,
-        spacesUsed: 3,
-        tripId: '0',
-        start: 'Campus San Joaquin',
-        end: 'Manquehue',
-        user: {
-          name: 'Benjamin',
-          reputation: 17,
-        },
-        status: 'accepted',
-      },
-      {
-        timestamp: 1571503602,
-        spacesUsed: 3,
-        tripId: '1',
-        start: 'Campus San Joaquin',
-        end: 'Manquehue',
-        user: {
-          name: 'Benjamin',
-          reputation: 17,
-        },
-        status: 'pending',
-      },
-      {
-        timestamp: 1571586402,
-        spacesUsed: 3,
-        tripId: '2',
-        start: 'Campus San Joaquin',
-        end: 'Manquehue',
-        user: {
-          name: 'Benjamin',
-          reputation: 17,
-        },
-        status: 'rejected',
-      },
-      {
-        timestamp: 1570985202,
-        spacesUsed: 3,
-        tripId: '3',
-        start: 'Campus San Joaquin',
-        end: 'Manquehue',
-        user: {
-          name: 'Benjamin',
-          reputation: 17,
-        },
-        status: 'accepted',
-      },
-      {
-        timestamp: 1571593602,
-        spacesUsed: 3,
-        tripId: '4',
-        start: 'Puente Alto',
-        end: 'Tobalaba',
-        user: {
-          name: 'Benjamin',
-          reputation: 17,
-        },
-        status: 'accepted',
-      },
-      {
-        timestamp: 1571676402,
-        spacesUsed: 3,
-        tripId: '5',
-        start: 'Tobalaba',
-        end: 'Puente Alto',
-        user: {
-          name: 'Benjamin',
-          reputation: 17,
-        },
-        status: 'accepted',
-      },
-    ]
-  }
-
-  async getTrips(token) {
+  async getTrips() {
     this.setState({ loading: true })
-
-    const etd = '2019-10-23T05:40:00.000Z'
-    console.log(ParseDate(etd))
-    console.log(ParseHour(etd))
-
-    await this.fetchTrips(token)
-      .then(trips => this.setState({ trips }))
-      .catch(err => {
-        this.setState({ loading: false })
-        alert('Hubo un error, intenta de nuevo más tarde', err)
-      })
-
-    this.setState({ loading: false })
-  }
-
-  async getTrips2() {
+    // console.log(this.props.user.token)
     const response = await this.props.fetchFutureTrips(12345) // this.props.user.token
-
+    console.log(typeof this.props.futureTrips)
     if (response.error) {
+      this.setState({ loading: false })
       alert(
         'Error obteniendo viajes',
         'Hubo un problema obteniendo los viajes. Por favor intentalo de nuevo.'
       )
     }
-    console.log(this.props.trips)
 
-    this.setState({ trips: this.props.trips })
+    this.setState({ trips: this.props.futureTrips.trips, loading: false })
   }
 
   render() {
-    const { loading } = this.state
-    const { trips } = this.state
+    const { loading, trips } = this.state
     console.log(this.state.trips)
     return (
       <div>
         {loading && <CircularProgress />}
         <Grid container spacing={2} justify="center" alignItems="center">
           {trips.map((trip, i) => {
-            console.log(trip)
-            console.log(trip.spacesUsed)
-            console.log('Enteredd')
             // Return the element. Also pass key
             return (
               <Grid item md={4} key={i}>
@@ -168,17 +64,12 @@ class MyTrips extends Component {
 }
 
 MyTrips.propTypes = {
-  isRequestedTrips: PropTypes.bool,
   fetchFutureTrips: PropTypes.func.isRequired,
   user: PropTypes.shape({
-    token: PropTypes.string.isRequired,
-    userId: PropTypes.string.isRequired,
+    token: PropTypes.string,
+    userId: PropTypes.string,
   }).isRequired,
-  trips: PropTypes.array,
-}
-
-MyTrips.defaultProps = {
-  isRequestedTrips: false,
+  futureTrips: PropTypes.arrayOf(PropTypes.object),
 }
 
 const mapDispatchToProps = dispatch => ({
@@ -187,6 +78,7 @@ const mapDispatchToProps = dispatch => ({
 
 const mapStateToProps = state => ({
   user: state.user,
+  futureTrips: state.futureTrips,
 })
 
 export default connect(
