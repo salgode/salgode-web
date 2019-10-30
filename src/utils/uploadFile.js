012ae0c5ffde45e2844b73b210b2321fae19b426
Yoav Navon <yanavon@uc.cl>
2019-10-29 22:33:04 -0300

    Fix: use last index to get filetype and remove spaces

--- src/utils/uploadFile.js
+++ src/utils/uploadFile.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 const uploadFile = async ref => {
   const file = ref.current.files[0]
-  const fileParts = file.name.split('.')
-  const fileName = fileParts[0]
-  const fileType = fileParts[1]
+  const dot_idx = file.name.lastIndexOf('.')
+  const fileName = file.name.substr(0, dot_idx).replace(/\s+/g, '')
+  const fileType = file.name.substr(dot_idx + 1)
   try {
     const res = await axios.post(
       `https://staging-api.salgode.com/upload/image`,
