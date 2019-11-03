47d2a4476a44a502374c71dabebf6f2a821c7adb
Juanfra <jfcampos1@uc.cl>
2019-11-02 23:16:54 -0300

    add: image input in create user

--- src/components/authentication/SignUpForm/index.js
+++ src/components/authentication/SignUpForm/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component, createRef } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
@@ -12,7 +12,14 @@ import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
 
 // utils
-import { formatPhone } from '../../../utils'
+import {
+  formatPhone,
+  notWrongPhone,
+  validPhone,
+} from '../../../utils/userInputs'
+import uploadFile from '../../../utils/uploadFile'
+import './style.sass'
+import Loading from '../../Loading/Loading'
 
 const styles = theme => ({
   '@global': {
@@ -40,7 +47,8 @@ const styles = theme => ({
     padding: '10px',
   },
 })
-class SignUpForm extends React.Component {
+
+class SignUpForm extends Component {
   constructor(props) {
     super(props)
 
@@ -51,6 +59,8 @@ class SignUpForm extends React.Component {
       phoneNumber: '',
       password: '',
       passwordRepeat: '',
+      selfie: '',
+      loading: false,
 
       validity: {
         name: false,
@@ -59,17 +69,29 @@ class SignUpForm extends React.Component {
         phoneNumber: false,
         password: false,
         passwordRepeat: false,
+        selfieImage: false,
+        idFront: false,
+        idBack: false,
       },
     }
-
+    this.selfieImage = createRef()
+    this.idFront = createRef()
+    this.idBack = createRef()
     this.onChangeName = this.onChangeName.bind(this)
     this.onChangeLastname = this.onChangeLastname.bind(this)
     this.onChangeEmail = this.onChangeEmail.bind(this)
     this.onChangePhoneNumber = this.onChangePhoneNumber.bind(this)
+    this.formatPhoneCL = this.formatPhoneCL.bind(this)
     this.onChangePassword = this.onChangePassword.bind(this)
     this.onChangePasswordRepeat = this.onChangePasswordRepeat.bind(this)
 
     this.getValidity = this.getValidity.bind(this)
+  }
+
+  formatPhoneCL() {
+    if (!/^\+56 9/g.test(this.state.phoneNumber)) {
+      this.setState({ phoneNumber: '+56 9' })
+    }
   }
 
   onChangeName({ target: { value: name } }) {
@@ -99,12 +121,14 @@ class SignUpForm extends React.Component {
   }
 
   onChangePhoneNumber({ target: { value: phoneNumber } }) {
-    phoneNumber = phoneNumber.replace(/ /g, '')
-    const validity = phoneNumber.match(/^[+]?\d{11}$/)
-    this.setState(oldState => ({
-      phoneNumber,
-      validity: { ...oldState.validity, phoneNumber: !!validity },
-    }))
+    if (notWrongPhone(phoneNumber)) {
+      phoneNumber = formatPhone(phoneNumber)
+      const validity = validPhone(phoneNumber)
+      this.setState(oldState => ({
+        phoneNumber: phoneNumber,
+        validity: { ...oldState.validity, phoneNumber: !!validity },
+      }))
+    }
   }
 
   onChangePassword({ target: { value: password } }) {
@@ -124,6 +148,40 @@ class SignUpForm extends React.Component {
     }))
   }
 
+  onChangeImage = name => event => {
+    if (event.currentTarget) {
+      this.setState(oldState => ({
+        validity: {
+          ...oldState.validity,
+          [name]: true,
+        },
+      }))
+    }
+  }
+
+  async onImageUpload(image) {
+    const response = await uploadFile(image)
+    if (response.error) {
+      return alert(
+        'Hubo un problema subiendo la imagen. Por favor intentalo de nuevo.'
+      )
+    }
+    return response.image_id
+  }
+
+  validateImagesExists() {
+    const preImage =
+      this.selfieImage.current !== null &&
+      this.idFront.current !== null &&
+      this.idBack.current !== null
+    const images =
+      preImage &&
+      this.selfieImage.current.files.length !== 0 &&
+      this.idFront.current.files.length !== 0 &&
+      this.idBack.current.files.length !== 0
+    return images
+  }
+
   getValidity() {
     const validity =
       this.state.validity.name &&
@@ -131,18 +189,30 @@ class SignUpForm extends React.Component {
       this.state.validity.email &&
       this.state.validity.phoneNumber &&
       this.state.validity.password &&
-      this.state.validity.passwordRepeat
+      this.state.validity.passwordRepeat &&
+      this.state.validity.selfieImage &&
+      this.state.validity.idFront &&
+      this.state.validity.idBack
     return validity && this.state.password === this.state.passwordRepeat
   }
 
-  handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter' && this.getValidity()) {
-      e.preventDefault()
-      const { onSubmit } = this.props
-      const { name, lastName, email, password, passwordRepeat } = this.state
-      let phoneNumber = this.state.phoneNumber
-      if (!phoneNumber.includes('+')) phoneNumber = '+' + phoneNumber
+  async submit() {
+    const { onSubmit } = this.props
+    const {
+      name,
+      lastName,
+      email,
+      password,
+      passwordRepeat,
+      phoneNumber,
+    } = this.state
+    if (this.getValidity() && this.validateImagesExists()) {
+      this.setState({ loading: true })
       const formatedPhone = formatPhone(phoneNumber)
+      const selfieLink = await this.onImageUpload(this.selfieImage)
+      const dniFrontLink = await this.onImageUpload(this.idFront)
+      const dniBackLink = await this.onImageUpload(this.idBack)
+      this.setState({ loading: false })
       onSubmit({
         name,
         lastName,
@@ -150,22 +220,37 @@ class SignUpForm extends React.Component {
         phone: formatedPhone,
         password,
         passwordRepeat,
+        selfieLink,
+        dniFrontLink,
+        dniBackLink,
       })
+    } else {
+      alert('Asegurate de tener todos los campos completos')
+    }
+  }
+
+  handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && this.getValidity()) {
+      e.preventDefault()
+      this.submit()
     }
   }
 
   render() {
-    const { onSubmit } = this.props
-    const { name, lastName, email, password, passwordRepeat } = this.state
-    let phoneNumber = this.state.phoneNumber
-    if (!phoneNumber.includes('+') && this.state.validity.phoneNumber)
-      phoneNumber = '+' + phoneNumber
-
-    const formatedPhone = formatPhone(phoneNumber)
+    const {
+      name,
+      lastName,
+      email,
+      password,
+      passwordRepeat,
+      phoneNumber,
+      loading,
+    } = this.state
     const { classes } = this.props
 
     return (
       <Container component="main">
+        {loading && <Loading />}
         <CssBaseline />
         <div className={classes.paper}>
           <Typography component="h1" variant="h5">
@@ -225,9 +310,10 @@ class SignUpForm extends React.Component {
                   fullWidth
                   label="Número de Teléfono"
                   type="tel"
-                  value={formatedPhone}
+                  value={phoneNumber}
                   onChange={this.onChangePhoneNumber}
                   onKeyPress={this.handleEnter}
+                  onFocus={this.formatPhoneCL}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -258,6 +344,46 @@ class SignUpForm extends React.Component {
                   onKeyPress={this.handleEnter}
                 />
               </Grid>
+              <Grid container item xs={12} md={12} justify="center">
+                <Grid item xs={12} sm={12}>
+                  <Typography className="image-center">Selfie</Typography>
+                </Grid>
+                <Grid item xs={12} sm={2}>
+                  <input
+                    type="file"
+                    ref={this.selfieImage}
+                    onChange={this.onChangeImage('selfieImage')}
+                  />
+                </Grid>
+              </Grid>
+              <Grid container item xs={12} md={12} justify="center">
+                <Grid item xs={12} sm={12}>
+                  <Typography className="image-center">
+                    Foto frontal Cédula de Identidad
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={2}>
+                  <input
+                    type="file"
+                    ref={this.idFront}
+                    onChange={this.onChangeImage('idFront')}
+                  />
+                </Grid>
+              </Grid>
+              <Grid container item xs={12} md={12} justify="center">
+                <Grid item xs={12} sm={12}>
+                  <Typography className="image-center">
+                    Foto trasera Cédula de Identidad
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={2}>
+                  <input
+                    type="file"
+                    ref={this.idBack}
+                    onChange={this.onChangeImage('idBack')}
+                  />
+                </Grid>
+              </Grid>
             </Grid>
             <Button
               type="button"
@@ -266,16 +392,7 @@ class SignUpForm extends React.Component {
               color="secondary"
               className={classes.submit}
               disabled={!this.getValidity()}
-              onClick={() =>
-                onSubmit({
-                  name,
-                  lastName,
-                  email,
-                  phone: formatedPhone,
-                  password,
-                  passwordRepeat,
-                })
-              }
+              onClick={() => this.submit()}
             >
               Registrarse
             </Button>
