68478ae4189995bd643c2c7f53da98713cf8c0a8
Verner Codoceo <vacodoceo@uc.cl>
2019-11-05 01:47:10 -0300

    Add error layout and handling

--- src/screens/ValidateEmail/index.js
+++ src/screens/ValidateEmail/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import Loading from '../../components/Loading/Loading'
 import { Container, Typography } from '@material-ui/core'
 import DoneOutlineIcon from '@material-ui/icons/DoneOutline'
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline'
 import { withStyles } from '@material-ui/core/styles'
 import './style.sass'
 
@@ -20,6 +21,9 @@ const styles = theme => ({
     flexDirection: 'column',
     alignItems: 'center',
     marginTop: '25vh',
+  },
+  error: {
+    color: '#d50000',
   },
   icon: {
     fontSize: 100,
@@ -46,7 +50,7 @@ class ValidateEmail extends Component {
 
   componentDidMount() {
     axios
-      .get('http://www.mocky.io/v2/5dc0ed2b3300006f001a4d6f')
+      .get('http://www.mocky.io/v2/5dc0ed2b3300006f001a4d6f1')
       .then(() => {
         this.setState({ loading: false })
       })
@@ -56,10 +60,26 @@ class ValidateEmail extends Component {
   }
 
   render() {
-    const { loading } = this.state
+    const { loading, error } = this.state
     const { classes } = this.props
     if (loading) {
       return <Loading />
+    }
+    if (error) {
+      return (
+        <Container className={classes.container}>
+          <ErrorOutlineIcon className={[classes.icon, classes.error]} />
+          <Container>
+            <Typography align="center" className={classes.title}>
+              Error de confirmación
+            </Typography>
+            <Typography align="center" className={classes.subtitle}>
+              Hubo un error al confirmar tu correo. Si el error persiste,
+              contacta a hello@salgode.com
+            </Typography>
+          </Container>
+        </Container>
+      )
     }
     return (
       <Container className={classes.container}>
