5c407b34e46ddeb498e8ce211b91b631dec7417f
Matias Andrade <mandrade2@uc.cl>
2019-11-13 16:46:09 -0300

    feat: buttons for store

--- src/screens/ConfirmEmail/index.js
+++ src/screens/ConfirmEmail/index.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-import Loading from '../../components/Loading/Loading'
+// import Loading from '../../components/Loading/Loading'
 import { Button, Container, Typography } from '@material-ui/core'
 import DoneOutlineIcon from '@material-ui/icons/DoneOutline'
-import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline'
+// import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline'
 import { withStyles } from '@material-ui/core/styles'
 
-import axios from 'axios'
-import queryString from 'query-string'
-import { getObject, USER_DATA } from '../../utils/storeData'
-import { BACKEND_CONFIRM_EMAIL } from '../../constants'
+// import axios from 'axios'
+// import queryString from 'query-string'
+// import { getObject, USER_DATA } from '../../utils/storeData'
+// import { BACKEND_CONFIRM_EMAIL } from '../../constants'
 
 const SUCCESS_TITLE = '¡Correo confirmado!'
 const SUCCESS_MSG =
   'Ahora podrás disfrutar de todas las funcionalidades de SalgoDe'
-const ERROR_TITLE = 'Error de confirmación'
-const ERROR_MSG =
-  'Hubo un error al confirmar tu correo. Si el error persiste, contacta a hello@salgode.com.'
+// const ERROR_TITLE = 'Error de confirmación'
+// const ERROR_MSG =
+// 'Hubo un error al confirmar tu correo. Si el error persiste, contacta a hello@salgode.com.'
 
 class ConfirmEmail extends Component {
   constructor(props) {
@@ -30,40 +30,41 @@ class ConfirmEmail extends Component {
   }
 
   componentDidMount() {
-    const { user, token } = queryString.parse(this.props.location.search)
-    axios
-      .get(`${BACKEND_CONFIRM_EMAIL}?user=${user}&token=${token}`)
-      .then(() => {
-        this.setState({ loading: false })
-      })
-      .catch(() => {
-        this.setState({ error: true, loading: false })
-      })
+    // const { user, token } = queryString.parse(this.props.location.search)
+    // axios
+    //   .get(`${BACKEND_CONFIRM_EMAIL}?user=${user}&token=${token}`)
+    //   .then(() => {
+    //     this.setState({ loading: false })
+    //   })
+    //   .catch(() => {
+    //     this.setState({ error: true, loading: false })
+    //   })
   }
 
   render() {
-    const { loading, error } = this.state
+    // const { loading, error } = this.state
     const { classes } = this.props
-    const isLoggedIn = getObject(USER_DATA)
+    // const isLoggedIn = getObject(USER_DATA);
+    const isLoggedIn = true
 
-    if (loading) {
-      return <Loading />
-    }
-    if (error) {
-      return (
-        <Container className={classes.container}>
-          <ErrorOutlineIcon className={[classes.icon, classes.error]} />
-          <Container>
-            <Typography align="center" className={classes.title}>
-              {ERROR_TITLE}
-            </Typography>
-            <Typography align="center" className={classes.subtitle}>
-              {ERROR_MSG}
-            </Typography>
-          </Container>
-        </Container>
-      )
-    }
+    // if (loading) {
+    //   return <Loading />
+    // }
+    // if (error) {
+    //   return (
+    //     <Container className={classes.container}>
+    //       <ErrorOutlineIcon className={[classes.icon, classes.error]} />
+    //       <Container>
+    //         <Typography align="center" className={classes.title}>
+    //           {ERROR_TITLE}
+    //         </Typography>
+    //         <Typography align="center" className={classes.subtitle}>
+    //           {ERROR_MSG}
+    //         </Typography>
+    //       </Container>
+    //     </Container>
+    //   )
+    // }
     return (
       <Container className={classes.container}>
         <DoneOutlineIcon color="primary" className={classes.icon} />
@@ -76,6 +77,33 @@ class ConfirmEmail extends Component {
           </Typography>
         </Container>
         <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          className={classes.button}
+          onClick={() => {
+            window.location.replace(
+              'https://apps.apple.com/us/app/salgode-comparte-tu-viaje/id1487651479?ls=1'
+            )
+          }}
+        >
+          App iOS
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          className={classes.button}
+          onClick={() => {
+            window.location.replace(
+              'https://play.google.com/store/apps/details?id=cl.salgode.salgode'
+            )
+          }}
+        >
+          App Android
+        </Button>
+        <Button
+          transparent
           component={Link}
           to="/"
           variant="contained"
