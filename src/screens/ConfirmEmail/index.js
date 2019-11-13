073ed3eb383fb8836d3a0d4ce812db6e506a0212
Matias Andrade <mandrade2@uc.cl>
2019-11-13 19:48:59 -0300

    fix: prettier

--- src/screens/ConfirmEmail/index.js
+++ src/screens/ConfirmEmail/index.js
@@ -1,54 +1,53 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-import Loading from "../../components/Loading/Loading";
-import { Button, Container, Typography } from "@material-ui/core";
-import DoneOutlineIcon from "@material-ui/icons/DoneOutline";
-import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
-import { withStyles } from "@material-ui/core/styles";
+import Loading from '../../components/Loading/Loading'
+import { Button, Container, Typography } from '@material-ui/core'
+import DoneOutlineIcon from '@material-ui/icons/DoneOutline'
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline'
+import { withStyles } from '@material-ui/core/styles'
 
-import axios from "axios";
-import queryString from "query-string";
-import { getObject, USER_DATA } from "../../utils/storeData";
-import { BACKEND_CONFIRM_EMAIL } from "../../constants";
+import axios from 'axios'
+import queryString from 'query-string'
+import { getObject, USER_DATA } from '../../utils/storeData'
+import { BACKEND_CONFIRM_EMAIL } from '../../constants'
 
-const SUCCESS_TITLE = "¡Correo confirmado!";
+const SUCCESS_TITLE = '¡Correo confirmado!'
 const SUCCESS_MSG =
-  "Ahora podrás disfrutar de todas las funcionalidades de SalgoDe";
-const ERROR_TITLE = "Error de confirmación";
+  'Ahora podrás disfrutar de todas las funcionalidades de SalgoDe'
+const ERROR_TITLE = 'Error de confirmación'
 const ERROR_MSG =
-  "Hubo un error al confirmar tu correo. Si el error persiste, contacta a hello@salgode.com.";
+  'Hubo un error al confirmar tu correo. Si el error persiste, contacta a hello@salgode.com.'
 
 class ConfirmEmail extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       loading: true,
-      error: false
-    };
+      error: false,
+    }
   }
 
   componentDidMount() {
-    const { user, token } = queryString.parse(this.props.location.search);
+    const { user, token } = queryString.parse(this.props.location.search)
     axios
       .get(`${BACKEND_CONFIRM_EMAIL}?user=${user}&token=${token}`)
       .then(() => {
-        this.setState({ loading: false });
+        this.setState({ loading: false })
       })
       .catch(() => {
-        this.setState({ error: true, loading: false });
-      });
+        this.setState({ error: true, loading: false })
+      })
   }
 
   render() {
-    const { loading, error } = this.state;
-    const { classes } = this.props;
-    const isLoggedIn = getObject(USER_DATA);
-    const isLoggedIn = true;
+    const { loading, error } = this.state
+    const { classes } = this.props
+    const isLoggedIn = getObject(USER_DATA)
 
     if (loading) {
-      return <Loading />;
+      return <Loading />
     }
     if (error) {
       return (
@@ -63,7 +62,7 @@ class ConfirmEmail extends Component {
             </Typography>
           </Container>
         </Container>
-      );
+      )
     }
     return (
       <Container className={classes.container}>
@@ -83,8 +82,8 @@ class ConfirmEmail extends Component {
           className={classes.button}
           onClick={() => {
             window.location.replace(
-              "https://apps.apple.com/us/app/salgode-comparte-tu-viaje/id1487651479?ls=1"
-            );
+              'https://apps.apple.com/us/app/salgode-comparte-tu-viaje/id1487651479?ls=1'
+            )
           }}
         >
           App iOS
@@ -96,8 +95,8 @@ class ConfirmEmail extends Component {
           className={classes.button}
           onClick={() => {
             window.location.replace(
-              "https://play.google.com/store/apps/details?id=cl.salgode.salgode"
-            );
+              'https://play.google.com/store/apps/details?id=cl.salgode.salgode'
+            )
           }}
         >
           App Android
@@ -111,48 +110,48 @@ class ConfirmEmail extends Component {
           size="large"
           className={classes.button}
         >
-          {isLoggedIn.token ? "Buscar viajes" : "Iniciar sesión"}
+          {isLoggedIn.token ? 'Buscar viajes' : 'Iniciar sesión'}
         </Button>
       </Container>
-    );
+    )
   }
 }
 
 ConfirmEmail.propTypes = {
   classes: PropTypes.object.isRequired,
-  location: PropTypes.object
-};
+  location: PropTypes.object,
+}
 
 const styles = theme => ({
-  "@global": {
+  '@global': {
     body: {
-      backgroundColor: theme.palette.common.white
-    }
+      backgroundColor: theme.palette.common.white,
+    },
   },
   button: {
-    marginTop: "10vh"
+    marginTop: '10vh',
   },
   container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    marginTop: "20vh"
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '20vh',
   },
   error: {
-    color: "#d50000"
+    color: '#d50000',
   },
   icon: {
-    fontSize: 100
+    fontSize: 100,
   },
   title: {
     fontWeight: 500,
     fontSize: 28,
     marginTop: 16,
-    marginBottom: 16
+    marginBottom: 16,
   },
   subtitle: {
-    fontWeight: 100
-  }
-});
+    fontWeight: 100,
+  },
+})
 
-export default withStyles(styles)(ConfirmEmail);
+export default withStyles(styles)(ConfirmEmail)
